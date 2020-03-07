5c97505ac4f15474eb6ecc1423d56afb0a465862
Kristina Arezina <arezina.kristina23@gmail.com>
2020-03-07 10:17:20 -0500

    working on fixing history

--- tic-tac-toe/src/components/TicTacToe.js
+++ tic-tac-toe/src/components/TicTacToe.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import Grid from './Grid';
+
 
 export default class TicTacToe extends Component {
+  constructor(props) {
+      super(props);
+      // set default state
+      this.state = {
+        // determines who is going next
+        xIsNext: true,
+        stepNumber: 0,
+
+        }
+      }
 
     render() {
+
       return(
-    <h1>Testing</h1>
-  )
+          <div className="ticTacToe">
+            <div className="ticTacToe-grid">
+              <Grid onClick={(i) => this.handleClick(i)}
+              boxs={current.boxs} />
+            </div>
+          </div>
+      )
     }
   }
