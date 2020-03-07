f7dcda43e264fa4b4887fc56d908ddb5121ed9ba
Kristina Arezina <arezina.kristina23@gmail.com>
2020-03-07 10:01:53 -0500

    creating parts of the game and passing down data onclick events with props

---
+++ tic-tac-toe/src/components/Grid.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import Box from "./Box.js"
+
+export default class Grid extends Component {
+
+  // Onclick will send i as parameter
+  renderBox(i){
+    return <Box value={this.props.boxs[i]}
+    onClick={() => this.props.onClick(i)}
+    />
+  }
+
+  render() {
+    return(
+      <div>
+          <div>
+          <div className="grid-row">
+            {this.renderBox(0)}
+            {this.renderBox(1)}
+            {this.renderBox(2)}
+        </div>
+          <div className="grid-row">
+            {this.renderBox(3)}
+            {this.renderBox(4)}
+            {this.renderBox(5)}
+        </div>
+          <div className="grid-row">
+            {this.renderBox(6)}
+            {this.renderBox(7)}
+            {this.renderBox(8)}
+          </div>
+      </div>
+    )
+  }
